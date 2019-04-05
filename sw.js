5f8a3d91c6b9513e7f46557a4ff3aa8f15860e1e
unknown <almaqtarif@gmail.com>
2019-04-05 20:50:25 +0300

    fix cashing offline

--- sw.js
+++ sw.js
@@ -1,56 +1,84 @@
-const cacheFiles = [
-	'index.html',
-	'restaurant.html',
-	'/css/styles.css',
-	'/js/dbhelper.js',
-	'/js/main.js',
-	'/js/restaurant_info.js',
-	'/img/*'
-];
+// Define the Caches
+var staticCacheName = 'mws-restaurant-static-v';
+// Get Random number for Cache ID
+ var randomNumberBetween0and19999 = Math.floor(Math.random() * 20000);
+ var cache_id = randomNumberBetween0and19999;
+ staticCacheName += cache_id;
 
-//Installing service worker
-self.addEventListener("install", function(e) {
-	e.waitUntil(
-		caches.open('v1').then(function(cache) { 
-
-			return cache.addAll(cacheFiles);
-
-			
-		})
-
-	);
-
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+    return cache.addAll([
+	    'index.html',
+		'restaurant.html',
+		'/css/styles.css',
+		'/js/dbhelper.js',
+		'/js/main.js',
+		'/js/restaurant_info.js',
+		'/data/restaurant.json', 
+		'/img/*'
+    ])
+    .catch(error => {
+      
+    });
+  }));
+});
+//activate listner
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('mws-restaurant-') &&
+                 cacheName != staticCacheName;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
 });
 
-//fetch service worker
-self.addEventListener('fetch', function(e) {
+//fetch listner
+self.addEventListener('fetch', 
+function(event) 
+{
+  event.respondWith
+  (    
+    caches.match(event.request)
+    .then
+    (
+      function(response) 
+      {
+        if (response !== undefined) 
+        {
+          return response;
+        } 
+      
+        else 
+        {        
+          return fetch(event.request).then
+          (
+              function (response) 
+              {
+                let responseClone = response.clone();
+                
+                caches.open(staticCacheName)
+                .then
+                (
+                  function (cache) 
+                  {
+                    cache.put(event.request, responseClone);
+                  }
+                );
+                return response;
+              }
+          );
+        }
+      }
+    ) // end of promise for cache match
+      
+  ); // end of respond
 
-	e.respondWith(
-
-		caches.match(e.request).then(function(response){
-			if(response) {
-				console.log('Found ', e.request, 'in cache');
-				return response;
-			}
-			else {
-				console.log('Could not find ', e.request, 'in cache, Fetching ');
-				return fetch(e.request)
-				.then(function(response) {
-					const cloneResponse = response.clone();
-					caches.open('v1').then(function(cache) {
-						cache.put(e.request, cloneResponse);
-
-					})
-
-					return response;
-				})
-				.catch(function(err) {
-					console.log(err);
-				});
-			}//end else
-
-		})
-
-	);
-
-});
+}
+);
